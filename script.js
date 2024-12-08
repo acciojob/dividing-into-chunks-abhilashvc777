3a1e87e7c0baa4a8818375fc6b71117634fbe59e
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-09 01:23:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,26 +2,23 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
         const divide = (arr,n) => {
   // Write your code here
-  let arr2 = new Array()
-  let arr3=[]
-	for(let i=0; i<=arr.length-1; i++){
-		let arr1 = []
-		for(let j=i; j<=arr.length-1; j++){
-			 arr1.push(arr[j])
-            //  console.log(arr1)
-            // add_subarray.push(arr1)
-             let sum = 0;
-            for(let k=0; k<=arr1.length-1; k++){
-                sum=sum+arr1[k]
+        const arr1 = [];
+        let k = 0;
+        for(let i=0; i<=arr.length-1; i++){
+            let sum = 0;
+           for(let j=i; j<=arr.length-1; j++){
+            sum=sum+arr[j]
+            if(sum<=n){
+                k=j;
             }
-        if(sum<=n){
-            let a = arr1.length;
-            // console.log(arr1)
-           arr2.push(arr1.slice())
-            } 
-		}
-    }
-    return arr2;
+            if(sum>n){
+                break;
+            }
+           }
+           arr1.push(arr.slice(i,k+1))
+           i=k;
+        }
+			return arr1
 };
 
 const n = prompt("Enter n: ");
